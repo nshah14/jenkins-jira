ed317534c295fb3451c59e0dd457b87c439a3f1d
nshah14 <naved1482@gmail.com>
2018-10-05 09:48:38 +0100

    TEST-10: item url

--- project.py
+++ project.py
@@ -3,7 +3,7 @@ import subprocess
 import sys
 
 def main():
-    os.environ['PATH_TO_ARTIFCT'] = "my_path"
+    os.environ['PATH_TO_ARTIFCT'] = "itms-services://?action=download-manifest&url=https%3A%2F%2Fapp-store.king.com%2Fplist%3Ftoken%3D4a1447be-99bc-406a-9473-765c1abce09d"
     print os.environ.get('PATH_TO_ARTIFCT')
 
 if __name__ == '__main__':
